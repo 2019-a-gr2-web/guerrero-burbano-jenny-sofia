5f1a9f8a9b1c02e923db10bfe60e538b2b9d2ba4
jsofii <sofi-0106@hotmail.com>
2019-04-10 16:00:28 -0500

    decoradores

--- 01-http/02-servidor-web-nodejs/api-web/src/app.controller.ts
+++ 01-http/02-servidor-web-nodejs/api-web/src/app.controller.ts
@@ -1,10 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get,  Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, aP, aPri,aProt) {
+    this.atributoPublico =aP;
+    this.atributoProtegido=aProt;
+    this.atributoPrivado= aPri;
+  }
+  public MetodoPublico(){
 
+  }
+  protected MetodoProtegido(){
+
+  }
+  //comentario
+  private MetodoPrivado(){
+
+  }
+  @Get() //DEFFINO EL MÉTODO HTTP
+  getHello2(){
+    return 'HOLA MUNDO'
+  }
+
+  public atributoPublico;
+  private atributoPrivado;
+  protected atributoProtegido
   @Get()
   getHello(): string {
     return this.appService.getHello();
