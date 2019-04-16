6a2694b60ff890259a8be92666b48eafe07f8a58
jsofii <sofi-0106@hotmail.com>
2019-04-16 14:53:03 -0500

    Commit

--- 01-http/02-servidor-web-nodejs/api-web/src/app.controller.ts
+++ 01-http/02-servidor-web-nodejs/api-web/src/app.controller.ts
@@ -1,33 +1,54 @@
-import { Controller, Get,  Post } from '@nestjs/common';
+import {Controller, Get, Post, HttpCode, Put, Delete} from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/api')
 export class AppController {
-  constructor(private readonly appService: AppService, aP, aPri,aProt) {
-    this.atributoPublico =aP;
-    this.atributoProtegido=aProt;
-    this.atributoPrivado= aPri;
-  }
-  public MetodoPublico(){
+  constructor(private readonly appService: AppService) { }
 
-  }
-  protected MetodoProtegido(){
-
-  }
-  //comentario
-  private MetodoPrivado(){
-
-  }
-  @Get() //DEFFINO EL MÉTODO HTTP
-  getHello2(){
-    return 'HOLA MUNDO'
-  }
-
-  public atributoPublico;
-  private atributoPrivado;
-  protected atributoProtegido
-  @Get()
+  @Get('/HelloWorld')
   getHello(): string {
-    return this.appService.getHello();
+    return 'HELLO WORLD';
   }
+
+  //cuando se hace cambio en la logica del servidor se debe reiniciar el servidor
+  //para ello se puede usar npm run start:dev
+
+  @Get('/HolaMundo')//alt+enter para importar arriba directamente
+  @HttpCode(205)
+  postHolaMundo() {
+    return 'Hola mundo en post';
+  }
+  @Put('/Bonjour_le_monde')
+  bonjour(): string{
+    return '\n' +
+        'Bonjour le monde'
+  }
+  @Delete('/Ciao_mondo')
+  CiaoMondo(): string{
+    return '\n' +
+        'Italiano'
+  }
+
+
 }
+
+/*
+@nombreDecoradorClase //antes de instaciar clases, metodos, atributos. Un decorador es una función, Se ejecuta antes de crearse. DECORADPR->Funcion
+class clase{
+  @decoradorAtributo
+  public publico;
+  private privado;
+  protected protegido;
+  constructor(@parametro()publ,priv,prot){
+    this.publico=publ;
+    this.privado=priv;
+    this.protegido=prot;
+  }
+  @metodoA
+  public metodopublico(){
+  }
+    @metodob
+  private metodoprovado(){}
+    @metodoC //und decorador es una función que se ejecuta antes de algo(clases,parametros,metodoso,atributos)
+  protected metodoprotected(){}
+}*/
