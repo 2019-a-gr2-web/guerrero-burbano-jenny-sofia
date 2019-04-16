e799eefd0948ebf4dad85ea57a10a171fc8cb10f
jsofii <sofi-0106@hotmail.com>
2019-04-16 16:00:56 -0500

    Decoradores/ Get/Post/Delete/Put

--- 01-http/02-servidor-web-nodejs/api-web/src/app.controller.ts
+++ 01-http/02-servidor-web-nodejs/api-web/src/app.controller.ts
@@ -1,5 +1,7 @@
-import {Controller, Get, Post, HttpCode, Put, Delete} from '@nestjs/common';
+import {Controller, Get, Post, HttpCode, Put, Delete, Headers} from '@nestjs/common';
 import { AppService } from './app.service';
+import {ok} from "assert";
+import any = jasmine.any;
 
 @Controller('/api')
 export class AppController {
@@ -7,13 +9,19 @@ export class AppController {
 
   @Get('/Hello-World')
   getHello(): string {
+    var nombre= 'Sofía'
+    var edad= 21
+    var sueldo= 1.20;
+    var casado= false
+    var hijos= null
+    var alas= undefined
     return 'HELLO WORLD';
   }
 
   //cuando se hace cambio en la logica del servidor se debe reiniciar el servidor
   //para ello se puede usar npm run start:dev
 
-  @Post('/Hola-Mundo')//alt+enter para importar arriba directamente
+  @Post('/hola-mundo')//alt+enter para importar arriba directamente
   hola() {
     return 'Hola mundo en post';
   }
@@ -27,9 +35,45 @@ export class AppController {
     return '\n' +
         'Ciao_mondo'
   }
+  @Get('/adivina')
+  adivina(@Headers() headers){
+    console.log('Headers', headers.numero)
+    const numeroRandomico=  Math.round(Math.random()*10)
+    const numeroCabecera= Number(headers.numero)
+    if(numeroCabecera == numeroRandomico){
+      return ':)'
+    }else{
+      return ':('
+    }
+
+  }
 
 
 }
+const json=[
+  {
+    Nombre: "Sofia",
+    Key : "value ",
+    "edad" : any,
+    "sueldo:": 10.21,
+    "casado": false,
+    "hijos": ["Micky ", 1,1.01, null,{
+      "nombre": "Micky Guerrero"
+    }]
+  }
+];
+let objeto: any ={
+  propiedad: 'valor',
+  propiedadDos: 'valor2'
+}
+objeto.propiedad
+
+objeto.propiedad3= 'valor3';
+objeto['propiedadTres']= 'valor3';
+delete objeto.propiedadTres;
+objeto.propiedadTres= undefined;
+
+
 
 /*
 @nombreDecoradorClase //antes de instaciar clases, metodos, atributos. Un decorador es una función, Se ejecuta antes de crearse. DECORADPR->Funcion
