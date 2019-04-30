b28c4543c96af4a76310a5552da59f04d432c111
jsofii <sofi-0106@hotmail.com>
2019-04-30 15:46:34 -0500

    Ejemplos de operadores

--- 01-http/02-servidor-web-nodejs/api-web/src/main.ts
+++ 01-http/02-servidor-web-nodejs/api-web/src/main.ts
@@ -6,6 +6,8 @@ const cookieParser = require('cookie-parser');
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.use(cookieParser('Secreto'));
+    // @ts-ignore
+    app.set('view engine', 'ejs');
     // app.set('view engine', 'ejs');
     await app.listen(3002);
 }
