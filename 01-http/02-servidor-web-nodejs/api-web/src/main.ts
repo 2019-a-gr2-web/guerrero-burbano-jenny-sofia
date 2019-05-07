c6384a35eb3ac8ab285052f9b455c3e799e736fb
jsofii <sofi-0106@hotmail.com>
2019-05-07 15:58:47 -0500

    ejs

--- 01-http/02-servidor-web-nodejs/api-web/src/main.ts
+++ 01-http/02-servidor-web-nodejs/api-web/src/main.ts
@@ -2,13 +2,17 @@ import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 // import * as cookieParser from 'cookie-parser'
 const cookieParser = require('cookie-parser');
-
+import * as express from 'express';
+import * as path from 'path';
+import * as favicon from 'serve-favicon';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.use(favicon(path.join(__dirname, '..', 'publico', 'imagenes', 'perrito.ico')))
     app.use(cookieParser('Secreto'));
     // @ts-ignore
     app.set('view engine', 'ejs');
     // app.set('view engine', 'ejs');
+    app.use(express.static('publico'));
     await app.listen(3002);
 }
 
